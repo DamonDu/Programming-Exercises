b3538e5cefa027fe8dc7e879655531429ffe1e18
DamonDu <sy20030260@163.com>
2017-10-14 23:18:10 +0800

    Sqrt(x); Binary search

--- Leetcode.69.java
+++ Leetcode.69.java
@@ -1,5 +1,6 @@
 import java.lang.*;
 
+//My Solution
 class Solution {
     public int mySqrt(int x) {
      	int min = 0;
@@ -20,3 +21,22 @@ class Solution {
     	return min;
     }
 }
+
+//A Better Solution
+/*
+public int sqrt(int x) {
+    if (x == 0)
+        return 0;
+    int left = 1, right = Integer.MAX_VALUE;
+    while (true) {
+        int mid = left + (right - left)/2;
+        if (mid > x/mid) {
+            right = mid - 1;
+        } else {
+            if (mid + 1 > x/(mid + 1))
+                return mid;
+            left = mid + 1;
+        }
+    }
+}
+ */
